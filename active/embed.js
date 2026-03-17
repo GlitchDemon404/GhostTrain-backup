e0606facc881e8f573673a6d452f93b40af3d564
leah <leahconor91@gmail.com>
2026-03-17 16:53:45 -0400

    Update embed.js

--- active/embed.js
+++ active/embed.js
@@ -1,4 +1,5 @@
 "use strict";
+
 let urlParams = new URLSearchParams(window.location.search);
 let destination = urlParams.get("url");
 
@@ -6,28 +7,18 @@ if (!destination) {
   alert("Error: No URL provided!");
   throw new Error("No URL provided");
 }
-try {
-  destination = decodeURIComponent(destination);
-} catch (e) {
-  console.warn("Already decoded or invalid encoding");
-}
-if (!destination.startsWith("http://") && !destination.startsWith("https://")) {
-  destination = "https://" + destination;
-}
+
 try {
   destination = new URL(destination).toString();
 } catch (err) {
   alert(`Your boat crashed!\nInvalid URL:\n${err}`);
   throw err;
 }
-const text = document.getElementById("loading-text");
-if (text) text.innerText = "Launching Ghost Train...";
+
 registerSW()
   .then(() => {
-    const encoded = __uv$config.encodeUrl(destination);
-    window.location.href = __uv$config.prefix + encoded;
+    window.open(__uv$config.prefix + __uv$config.encodeUrl(destination), "_self");
   })
   .catch((err) => {
     alert(`Your boat crashed!\nAn error occurred:\n${err}`);
-    console.error(err);
   });
